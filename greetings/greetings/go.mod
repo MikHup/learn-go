e8543326fe54c07d22dee23953b95e4145362c53
Michael Christensen <mc16business@gmail.com>
2021-06-05 14:31:19 +0200

    added greetings folder

---
+++ greetings/greetings/go.mod
@@ -0,0 +1,3 @@
+module github.com/MikHup/learn-go
+
+go 1.16
--- greetings/hello/hello.go
+++ greetings/hello/hello.go
@@ -2,5 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/MikHup/learn-go"
+
+	"github.com/MikHup/learn-go/greetings/greetings"
 )
+
+func main() {
+	message := greetings.Hello("Michael")
+	fmt.Println(message)
+}
